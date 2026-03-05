4c79808e787b2036f23a72dcb574fedae1e45539
madhavavula7 <madhavavula7@gmail.com>
2026-03-05 18:44:16 +0530

    changing the table name

--- src/main/java/in/bookstore/main/entity/User.java
+++ src/main/java/in/bookstore/main/entity/User.java
@@ -7,10 +7,12 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "app_user")
 public class User {
 
 	@Id
