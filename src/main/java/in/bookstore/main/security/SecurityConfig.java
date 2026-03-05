d8f45d5956e7591beba6d532e3c0ff888e86cb95
madhavavula7 <madhavavula7@gmail.com>
2026-03-05 16:30:35 +0530

    added changes on cors

--- src/main/java/in/bookstore/main/security/SecurityConfig.java
+++ src/main/java/in/bookstore/main/security/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://*.vercel.app"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
 		configuration.setAllowCredentials(true);
