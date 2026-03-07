363e4a360976733e33496b5d8986b9b97819353f
madhavavula7 <madhavavula7@gmail.com>
2026-03-07 13:02:25 +0530

    make changes on entities

--- src/main/java/in/bookstore/main/entity/OrderItem.java
+++ src/main/java/in/bookstore/main/entity/OrderItem.java
@@ -1,9 +1,13 @@
 package in.bookstore.main.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
@@ -15,13 +19,16 @@ public class OrderItem {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "book_id")
 	private Book book;
 
 	private Integer quantity;
 	private Double price;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+	@JsonIgnore
 	private Order order;
 
 	public Long getId() {
